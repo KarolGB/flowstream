cd5194b07c3a4ecba65f354f8fb299c68b340518
KarolGB <karolgonzalezbinczak@gmail.com>
2026-04-01 09:52:29 +0200

    Added progress bar to player

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -12,8 +12,10 @@ import { usePlayer } from '../../context/PlayerContext';
 
 
 export default function TabsLayout() {
-  const { play, pause, isPlaying, currentTrack, next } = usePlayer()
-
+  const { play, pause, isPlaying, currentTrack, next, currentTime } = usePlayer()
+  const duration = currentTrack?.duration_seconds || 1
+  let progressPercentage = (currentTime / duration) * 100
+  progressPercentage = Math.min(100, Math.max(0, progressPercentage));
   return (
     <View className="flex-1 bg-neutral-950">
       <Tabs
@@ -66,36 +68,46 @@ export default function TabsLayout() {
       </Tabs>
 
       {currentTrack && (
-        <View className='absolute bottom-[65px] w-[96%] self-center flex-row items-center bg-neutral-900 rounded-xl p-2 border border-neutral-800 shadow-lg shadow-black z-10'>
+        <View className='absolute bottom-[65px] w-[96%] self-center shadow-lg shadow-black z-10'>
 
-          <Image
-            className='w-12 h-12 rounded-md bg-neutral-800'
-            source={{ uri: currentTrack?.thumbnail }}
-          />
+          <View className='w-full flex-row items-center bg-neutral-900 rounded-xl p-2 border border-neutral-800 overflow-hidden'>
 
-          <View className='justify-center ml-3 flex-1'>
-            <Text className='text-white font-bold text-sm' numberOfLines={1}>{currentTrack?.title}</Text>
-            <Text className='text-fuchsia-500 text-xs' numberOfLines={1}>{currentTrack?.artist}</Text>
+            <Image
+              className='w-12 h-12 rounded-md bg-neutral-800'
+              source={{ uri: currentTrack?.thumbnail }}
+            />
+
+            <View className='justify-center ml-3 flex-1'>
+              <Text className='text-white font-bold text-sm' numberOfLines={1}>{currentTrack?.title}</Text>
+              <Text className='text-fuchsia-500 text-xs' numberOfLines={1}>{currentTrack?.artist}</Text>
+            </View>
+
+            <View className='flex-row gap-4 items-center mr-2'>
+              <TouchableOpacity>
+                <Foundation name="previous" size={28} color="#d946ef" />
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={isPlaying ? pause : play}>
+                {isPlaying ? (
+                  <MaterialIcons name="pause-circle" size={36} color="#d946ef" />
+                ) : (
+                  <MaterialIcons name="play-circle" size={36} color="#d946ef" />
+                )}
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={next}>
+                <Entypo name="controller-next" size={28} color="#d946ef" />
+              </TouchableOpacity>
+            </View>
+
+            <View className="absolute bottom-0 left-0 right-0 h-[2px] bg-neutral-800">
+              <View
+                className="h-full bg-fuchsia-500"
+                style={{ width: `${progressPercentage}%` }}
+              />
+            </View>
+
           </View>
-
-          <View className='flex-row gap-4 items-center mr-2'>
-            <TouchableOpacity>
-              <Foundation name="previous" size={28} color="#d946ef" />
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={isPlaying ? pause : play}>
-              {isPlaying ? (
-                <MaterialIcons name="pause-circle" size={36} color="#d946ef" />
-              ) : (
-                <MaterialIcons name="play-circle" size={36} color="#d946ef" />
-              )}
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={next}>
-              <Entypo name="controller-next" size={28} color="#d946ef" />
-            </TouchableOpacity>
-          </View>
-
         </View>
       )}
 
