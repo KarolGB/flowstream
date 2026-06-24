8277d7232fdf5d318c52a746d4f6db494ac9084d
KarolGB <karolgonzalezbinczak@gmail.com>
2026-06-25 00:51:09 +0200

    Added Auth Router, hide docs in production

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from core.config import PRODUCTION
+
 import uvicorn
 
-app = FastAPI(title="Flowstream API")
+#Routers ###########################################
+from routers.auth.router import router as auth_router
+
+app = FastAPI(
+    title="Flowstream API",
+    docs_url=None if PRODUCTION else "/docs",
+    redoc_url=None if PRODUCTION else "/redoc",
+    openapi_url=None if PRODUCTION else "/openapi.json",
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router=auth_router)
 
 @app.get("/")
 def root():
