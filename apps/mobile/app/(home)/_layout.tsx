8b6823b3a2c15b47e8ecc7facc1a7752bc556eaa
KarolGB <karolgonzalezbinczak@gmail.com>
2026-07-09 15:18:45 +0200

    Added logout button in home page

--- apps/mobile/app/(home)/_layout.tsx
+++ apps/mobile/app/(home)/_layout.tsx
@@ -1,13 +1,14 @@
 import { Tabs } from "expo-router"
 export default function Layout() {
     return (
-        <Tabs screenOptions={{
+        < Tabs screenOptions={{
             headerShown: false,
             tabBarStyle: { backgroundColor: "#0a0a0a" },
             tabBarActiveTintColor: "#fff",
             tabBarInactiveTintColor: "#888",
-        }}>
+        }
+        }>
             <Tabs.Screen name="home" options={{ title: "Home" }} />
-        </Tabs>
+        </Tabs >
     )
 }
--- apps/mobile/app/(home)/home.tsx
+++ apps/mobile/app/(home)/home.tsx
@@ -1,10 +1,16 @@
-import { Text } from "react-native"
-import { SafeAreaView } from "react-native-safe-area-context"
-
-export default function Home() {
+import { Pressable, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from "../../context/AuthContext";
+export default function home() {
+    const { logout } = useAuth();
     return (
         <SafeAreaView className="flex-1 items-center justify-center">
             <Text className=" text-2xl font-bold">Home</Text>
+            <Pressable className="bg-blue-500 rounded p-2 mt-4" onPress={async () => {
+                logout();
+            }}>
+                <Text className="text-white">Logout</Text>
+            </Pressable>
         </SafeAreaView>
     )
 }
