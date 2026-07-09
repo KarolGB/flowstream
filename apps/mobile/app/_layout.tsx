69ae895064da86c4c301ccfcfdbc8c54ea0165bc
KarolGB <karolgonzalezbinczak@gmail.com>
2026-07-09 13:09:20 +0200

    Added protected routed and showing Loading Spinner while loading api and auth

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,5 +1,5 @@
-import { router, Stack } from "expo-router"
-import { useEffect } from "react"
+import LoadingSpinner from "@/components/LoadingSpinner"
+import { Stack } from "expo-router"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import { ApiProvider, useApi } from "../context/ApiContext"
 import { AuthProvider, useAuth } from "../context/AuthContext"
@@ -9,35 +9,39 @@ function InitialLayout() {
     const { apiUrl, isLoading: apiLoading } = useApi()
     const { isAuthenticated, isLoading: authLoading } = useAuth()
 
-    useEffect(() => {
-        if (apiLoading || authLoading) return;
-        if (!apiUrl) {
-            router.replace("/setup")
-        } else if (!isAuthenticated) {
-            router.replace("/login")
-        } else {
-            router.replace("/home")
-        }
-    }, [apiUrl, isAuthenticated, apiLoading, authLoading])
+    if (apiLoading || authLoading) {
+        return (
+            <LoadingSpinner />
+        )
+    }
 
-    if (apiLoading || authLoading) return;
-
-    return <Stack screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: "#0a0a0a" },
-        animation: 'none'
-    }}
-    />
+    return (
+        <Stack screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: "#0a0a0a" },
+            animation: 'none'
+        }}>
+            <Stack.Protected guard={!apiUrl} >
+                <Stack.Screen name="setup" />
+            </Stack.Protected>
+            <Stack.Protected guard={!isAuthenticated}>
+                <Stack.Screen name="login" />
+            </Stack.Protected>
+            <Stack.Protected guard={isAuthenticated && !!apiUrl}>
+                <Stack.Screen name="(home)" />
+            </Stack.Protected>
+        </Stack>
+    )
 }
 
 export default function RootLayout() {
     return (
-        <SafeAreaProvider>
-            <ApiProvider>
-                <AuthProvider>
+        <ApiProvider>
+            <AuthProvider>
+                <SafeAreaProvider>
                     <InitialLayout />
-                </AuthProvider>
-            </ApiProvider>
-        </SafeAreaProvider>
+                </SafeAreaProvider>
+            </AuthProvider>
+        </ApiProvider>
     )
 }
